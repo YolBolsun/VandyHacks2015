471a1fd1e598a5539a073fd77a486ac1a64e5e78
unknown <jonathan.a.clinton@vanderbilt.edu>
2015-09-25 21:25:14 -0500

    added hillary and spawner

---
+++ HackerNight/HackerNightV2/Assets/spawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+    public GameObject hillary;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.frameCount % 60 == 0)
+        {
+            Instantiate(hillary, this.transform.position, new Quaternion());
+        }
+	}
+}
